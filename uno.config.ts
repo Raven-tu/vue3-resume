df830ec5f82c0ecb32b118a2eb082f503991d18f
raventu <80778915raventu@gmail.com>
2024-02-26 15:42:04 +0800

    feat: change project name

--- uno.config.ts
+++ uno.config.ts
@@ -13,6 +13,18 @@ export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['flex-cc', 'flex justify-center items-center'],
+    ['flex-rc', 'flex flex-col justify-center items-center'],
+    ['wh-full', 'w-full h-full'],
+    [
+      'page-base',
+      'page-tab-height flex flex-col py-3 flex-1 bg-page_bg box-border',
+    ],
+    ['absolute-lt', 'absolute left-0 top-0'],
+    ['absolute-lb', 'absolute left-0 bottom-0'],
+    ['absolute-rt', 'absolute right-0 top-0'],
+    ['absolute-rb', 'absolute right-0 bottom-0'],
+
   ],
   presets: [
     presetUno(),
