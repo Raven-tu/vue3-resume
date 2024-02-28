9e06d0e82cdba6192a96daa14ad75415336e6b71
raventu <80778915raventu@gmail.com>
2024-02-28 09:39:07 +0800

    fix: Fix the missing icon

--- src/components/TheSkills.vue
+++ src/components/TheSkills.vue
@@ -11,6 +11,37 @@ const skillsData = computed({
     currentData.value.skills = value
   },
 })
+
+const skillList = ref([
+  { name: 'JavaScript', icon: 'i-vscode-icons-file-type-js-official' },
+  { name: 'TypeScript', icon: 'i-vscode-icons-file-type-typescript-official' },
+  { name: 'React/Next.js/Remix', icon: 'i-vscode-icons-file-type-reactjs' },
+  { name: 'Node.js', icon: 'i-vscode-icons-file-type-node' },
+  { name: 'GraphQL', icon: 'i-vscode-icons-file-type-graphql' },
+  { name: 'Relay', icon: 'i-vscode-icons-folder-type-library' },
+  { name: 'WebRTC', icon: '' },
+  { name: 'Docker', icon: 'i-vscode-icons-file-type-docker2' },
+  { name: 'Kubernetes', icon: 'i-vscode-icons-folder-type-kubernetes' },
+  { name: 'MongoDB', icon: 'i-vscode-icons-folder-type-mongodb-opened' },
+  { name: 'MySQL', icon: 'i-vscode-icons-file-type-mysql' },
+  { name: 'Redis', icon: 'i-vscode-icons-folder-type-redis' },
+  { name: 'Nginx', icon: 'i-vscode-icons-file-type-nginx' },
+  { name: 'AWS', icon: 'i-vscode-icons-folder-type-aws' },
+  { name: 'GCP', icon: 'i-vscode-icons-folder-type-gcp' },
+  { name: 'Azure', icon: 'i-vscode-icons-folder-type-azure' },
+  { name: 'Git', icon: 'i-vscode-icons-folder-type-git' },
+  { name: 'GitHub', icon: 'i-vscode-icons-folder-type-github' },
+  { name: 'GitLab', icon: 'i-vscode-icons-folder-type-gitlab' },
+  { name: 'Vue', icon: 'i-vscode-icons-file-type-vue' },
+  { name: 'Nuxt', icon: 'i-vscode-icons-file-type-nuxt' },
+])
+
+const skillsIndex = skillList.value.map(item => item.name)
+
+function getSkillIcon(name: string) {
+  const index = skillsIndex.indexOf(name)
+  return index > -1 ? skillList.value[index].icon : ''
+}
 </script>
 
 <template>
@@ -20,13 +51,12 @@ const skillsData = computed({
     </h2>
     <div class="flex flex-wrap gap-1">
       <div
-        v-for="(item, index) in skillsData" :key="index"
-        dark="bg-gray-700"
-        class="focus:ring-ring text-secondary-foreground hover:bg-secondary/60 inline-flex items-center border border-transparent rounded-md bg-gray-200 px-2 py-0.5 text-nowrap align-middle text-xs font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+        v-for="(item, index) in skillsData" :key="index" dark="bg-gray-700"
+        class="text-secondary-foreground hover:bg-secondary/60 focus:ring-ring inline-flex items-center border border-transparent rounded-md bg-gray-200 px-2 py-0.5 text-nowrap align-middle text-xs font-semibold font-mono transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
       >
         <span class="flex-cc gap-x-2">
-          <i v-show="item.icon" class="block h-4 w-4" :class="item.icon" />
-          <span> {{ item.name }}</span>
+          <i v-show="getSkillIcon(item)" class="block h-4 w-4" :class="getSkillIcon(item)" />
+          <span> {{ item }}</span>
         </span>
       </div>
     </div>
