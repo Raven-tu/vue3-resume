60bc83909ef7d4656031c0f4f66d56663edcadd4
raventu <80778915raventu@gmail.com>
2024-02-28 14:42:42 +0800

    release: v1.0.0

--- src/stores/resume.ts
+++ src/stores/resume.ts
@@ -98,7 +98,7 @@ export const useResumestore = defineStore('resume', () => {
     ],
   }
 
-  const currentData = useStorage('resume-storage', defaultData, localStorage, { mergeDefaults: true })
+  const currentData = useLocalStorage('resume-storage', defaultData, { mergeDefaults: true })
 
   async function importData(data: typeof defaultData) {
     currentData.value = data
